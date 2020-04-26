2d34c576630a9bcece20092241cd69c2bb70c7ee
Trevor Sinnott <sinnott.trevor@gmail.com>
2020-04-26 10:36:49 +0900

    exercise 2.1: course contents step6 (correction)

--- part1/courseinfo/src/components/Content.js
+++ part1/courseinfo/src/components/Content.js
@@ -4,9 +4,9 @@ import Part from "./Part";
 const Content = ({ course }) => {
   return (
     <>
-      <Part part={course.parts[0]} />
-      <Part part={course.parts[1]} />
-      <Part part={course.parts[2]} />
+      {course.parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
     </>
   );
 };
